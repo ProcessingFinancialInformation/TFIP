b536acdc1b972ca7fcb6900ccbf776c8d99d8091
Edward Chernyavsky <e.chernyavsky@itransition.com>
2015-11-28 16:51:14 +0300

    Set up Autofac for Web.UI. First notification: New Request Created.

--- TFIP.Web.UI/Controllers/HomeController.cs
+++ TFIP.Web.UI/Controllers/HomeController.cs
@@ -1,12 +1,26 @@
 ﻿using System.Web.Mvc;
+using TFIP.Business.Contracts;
 
 namespace TFIP.Web.UI.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly INotificationService notificationService;
+
+        public HomeController(INotificationService notificationService)
+        {
+            this.notificationService = notificationService;
+        }
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult PostTestNotification()
+        {
+            notificationService.SendNewCreditRequestCreated(1, "1234");
+            return View("Index");
         }
     }
 }
--- TFIP.Web.UI/Global.asax.cs
+++ TFIP.Web.UI/Global.asax.cs
@@ -1,17 +1,24 @@
-﻿using System.Web.Mvc;
+﻿using System.Reflection;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using TFIP.Web.Infrastructure;
 
 namespace TFIP.Web.UI
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
-        protected void Application_Start()
+        protected override void Register()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected override Assembly GetWebAssembly()
+        {
+            return Assembly.GetExecutingAssembly();
         }
     }
 }
