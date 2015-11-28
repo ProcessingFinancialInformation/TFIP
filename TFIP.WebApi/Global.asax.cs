4e0f07859925dba4c020376af4f590bad0f3a575
Ledyaev Vladimir <ledyaev@tut.by>
2015-11-28 22:29:13 +0300

    Ad works!!! Yiiiiiiii

--- TFIP.WebApi/Global.asax.cs
+++ TFIP.WebApi/Global.asax.cs
@@ -18,6 +18,7 @@ namespace TFIP.Web.Api
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            RegisterRoles();
         }
 
         protected override Assembly GetWebAssembly()
