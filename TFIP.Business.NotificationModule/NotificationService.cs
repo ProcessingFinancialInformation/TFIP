b113d9367f5a582ddbe725aa1fa93310bb8ffc06
Edward Chernyavsky <e.chernyavsky@itransition.com>
2015-11-28 13:18:06 +0300

    Notification Transport main functionality. Configuration Helper.

---
+++ TFIP.Business.NotificationModule/NotificationService.cs
@@ -0,0 +1,20 @@
+﻿using TFIP.Business.NotificationModule.Contracts;
+using TFIP.Business.NotificationModule.EmailTransport;
+
+namespace TFIP.Business.NotificationModule
+{
+    public class NotificationService : INotificationService
+    {
+        private readonly IEmailTransport _emailTransport;
+
+        public NotificationService(IEmailTransport emailTransport)
+        {
+            _emailTransport = emailTransport;
+        }
+
+        public bool SendMail(NotificationData emailNotificationData)
+        {
+            return _emailTransport.SendMail(emailNotificationData);
+        }
+    }
+}
