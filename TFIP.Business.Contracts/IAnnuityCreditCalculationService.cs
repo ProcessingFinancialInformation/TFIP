92f50da776e31922efa9c9e64cffe5f9129de456
Edward Chernyavsky <e.chernyavsky@itransition.com>
2015-12-07 20:00:30 +0300

    Some draft items for Calculation Service

---
+++ TFIP.Business.Contracts/IAnnuityCreditCalculationService.cs
@@ -0,0 +1,7 @@
+﻿namespace TFIP.Business.Contracts
+{
+    public interface IAnnuityCreditCalculationService : ICreditCalculationService
+    {
+
+    }
+}
---
+++ TFIP.Business.Contracts/ICreditCalculationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using TFIP.Business.Entities;
+
+namespace TFIP.Business.Contracts
+{
+    public interface ICreditCalculationService
+    {
+        decimal CalculateCurrentMonthAmmount(int creditTerm, decimal creditRate, decimal totalAmount, IEnumerable<Payment> payments);
+
+        decimal CalculateBalance(decimal totalAmount, IEnumerable<Payment> payments);
+
+        decimal CalculateFine(IEnumerable<Payment> payments, DateTime creditRequestDate, decimal creditRate,
+            decimal totalAmount, int creditTerm);
+    }
+}
